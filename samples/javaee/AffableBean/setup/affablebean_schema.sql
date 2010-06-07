4d764d2c4da080e8fb3d4f1940157ab4c3123295
tgiunipero <tgiunipero@fe13c105-a357-6c57-8887-942404c1b39c>
2010-06-07 17:17:56 +0000

    updates to schema creation script.

--- samples/javaee/AffableBean/setup/affablebean_schema.sql
+++ samples/javaee/AffableBean/setup/affablebean_schema.sql
@@ -7,15 +7,13 @@
 -- author: tgiunipero
 --
 
-SET NAMES 'utf8' COLLATE 'utf8_general_ci';
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
 DROP SCHEMA IF EXISTS `affablebean` ;
-CREATE SCHEMA IF NOT EXISTS `affablebean` DEFAULT CHARACTER SET utf8 ;
-USE `affablebean`;
+CREATE SCHEMA IF NOT EXISTS `affablebean` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+USE `affablebean` ;
 
 -- -----------------------------------------------------
 -- Table `affablebean`.`customer`
@@ -33,6 +31,28 @@ CREATE  TABLE IF NOT EXISTS `affablebean`.`customer` (
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB
 COMMENT = 'maintains customer details';
+
+
+-- -----------------------------------------------------
+-- Table `affablebean`.`customer_order`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `affablebean`.`customer_order` ;
+
+CREATE  TABLE IF NOT EXISTS `affablebean`.`customer_order` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `amount` DECIMAL(6,2) NOT NULL ,
+  `date_created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  `confirmation_number` INT UNSIGNED NOT NULL ,
+  `customer_id` INT UNSIGNED NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_customer_order_customer` (`customer_id` ASC) ,
+  CONSTRAINT `fk_customer_order_customer`
+    FOREIGN KEY (`customer_id` )
+    REFERENCES `affablebean`.`customer` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+COMMENT = 'maintains customer order details';
 
 
 -- -----------------------------------------------------
@@ -75,41 +95,19 @@ COMMENT = 'contains product details';
 
 
 -- -----------------------------------------------------
--- Table `affablebean`.`customer_order`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `affablebean`.`customer_order` ;
-
-CREATE  TABLE IF NOT EXISTS `affablebean`.`customer_order` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `amount` DECIMAL(6,2) NOT NULL ,
-  `date_created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-  `customer_id` INT UNSIGNED NOT NULL ,
-  `confirmation_number` INT UNSIGNED NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `fk_order_customer` (`customer_id` ASC) ,
-  CONSTRAINT `fk_order_customer`
-    FOREIGN KEY (`customer_id` )
-    REFERENCES `affablebean`.`customer` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-COMMENT = 'maintains customer order details';
-
-
--- -----------------------------------------------------
 -- Table `affablebean`.`ordered_product`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `affablebean`.`ordered_product` ;
 
 CREATE  TABLE IF NOT EXISTS `affablebean`.`ordered_product` (
-  `order_id` INT UNSIGNED NOT NULL ,
+  `customer_order_id` INT UNSIGNED NOT NULL ,
   `product_id` INT UNSIGNED NOT NULL ,
   `quantity` VARCHAR(4) NOT NULL DEFAULT '1' ,
-  PRIMARY KEY (`order_id`, `product_id`) ,
-  INDEX `fk_ordered_product_order` (`order_id` ASC) ,
+  PRIMARY KEY (`customer_order_id`, `product_id`) ,
+  INDEX `fk_ordered_product_customer_order` (`customer_order_id` ASC) ,
   INDEX `fk_ordered_product_product` (`product_id` ASC) ,
-  CONSTRAINT `fk_ordered_product_order`
-    FOREIGN KEY (`order_id` )
+  CONSTRAINT `fk_ordered_product_customer_order`
+    FOREIGN KEY (`customer_order_id` )
     REFERENCES `affablebean`.`customer_order` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
